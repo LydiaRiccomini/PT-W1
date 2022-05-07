ad3a77f647935e43c89c65c4cda7569769e334a0
Lydia Riccomini <twist6550@gmail.com>
2022-05-07 17:58:43 -0500

    first commit being made

---
+++ src/Week1/WeekOneDemo.java
@@ -0,0 +1,26 @@
+package Week1;
+
+public class WeekOneDemo {
+
+}
+	
+	
+	public static void main(String[] args) {
+		new Week1Demo () .runTheStuff();
+	}
+	
+	private void runTheStuff() {
+		double softTopPrice = 2000.00;
+		double debitCardBalance = 4000.00;
+		int tireSize = 35;
+		int weightOfJeep = 3970;
+		char directionIAmGettingLostIn = 'E';
+		String lastKnownLocation = "Moab, UT";
+		float engineSizeInLiters = 2.0f;
+		
+		String
+		
+		
+	}
+	
+}
