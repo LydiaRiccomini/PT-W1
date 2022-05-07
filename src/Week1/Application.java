9d6bbcf515b54a8a0ad979be6a7ca586f1bd8fbc
Lydia Riccomini <twist6550@gmail.com>
2022-05-07 18:34:14 -0500

    second commit no video refrence

---
+++ src/Week1/Application.java
@@ -0,0 +1,24 @@
+package Week1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		//operator performs actions on operands
+		//+ - * /
+		int age = 34 * 2;
+		System.out.println(age);
+		boolean isAge30 = age == 30;
+		System.out.println(isAge30);
+		
+		System.out.println(age == 68);
+		
+		age = age +1;
+		System.out.println(age);
+		
+		String firstName = "Tommy";
+		String lastName = "Smith";
+		String fullName = firstName + " " + lastName; //concatenation
+		System.out.println(fullName);
+	}
+
+}
